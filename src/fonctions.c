db62a77853bc7a5eaec0f37bf35667d9a0c3fe10
Noé <57629078+GxPixel@users.noreply.github.com>
2021-04-28 12:53:00 +0200

    Update fonctions.c

--- src/fonctions.c
+++ src/fonctions.c
@@ -1,1 +1,21 @@
 #include "header.h"
+#include <stdio.h>
+
+int** alloc(int lignes, int colones){
+	int** tab;
+	
+	tab = malloc(lignes * sizeof(int*));
+	for(int i =0;i<lignes;i++){
+		tab[i] = malloc(colones * sizeof(int));
+	}
+	
+	return tab;
+}
+
+void init(int** tab, int lignes, int colones){
+	for(int i=0;i<lignes;i++){
+		for(int j=0;j<colones;j++){
+			scanf("%d",&tab[i][j]);
+		}
+	}
+}
