839eda3000000724734cff274cdfc5e6d516258b
Noé <57629078+GxPixel@users.noreply.github.com>
2021-04-28 12:52:25 +0200

    Update header.h

--- src/header.h
+++ src/header.h
@@ -1,6 +1,18 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/* Nom : Faucher Noé
+ * Date : 28/04/2021 12:22
+ * 
+ * Allocation du tableau
+ */
+int** alloc(int lignes, int colones);
 
+/* Nom : Faucher Noé
+ * Date : 28/04/2021 12:30
+ * 
+ * Initialisation du tableau
+ */
+void init(int** tab, int lignes, int colones);
 
 #endif
